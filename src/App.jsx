435907bbe7ddcf238da3c0bddb7cb20e2f5ef144
rexparmar <parmarrex114@gmail.com>
2025-12-11 16:54:03 -0500

    Login & Reister Page is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-import './App.css'
-
+import Login from "./Pages/Login"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import RegisterPage from "./Pages/RegisterPage";
 function App() {
 
   return (
-   <>
-   </>
-  )
+   <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<RegisterPage/>}/>
+        {/* <Route path="/dashboard" element={<Dashboard />} /> */}
+        {/* <Route path="*" element={<NotFound />} /> */}
+      </Routes>
+    </Router>
+  );
 }
 
 export default App
--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,11 +1,118 @@
-import React from 'react'
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate, Link } from "react-router-dom";
 
-const Login = () => {
+function Login() {
+  const navigate = useNavigate();
+  const [state, setState] = React.useState("login");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function handleLogin(e) {
+    e.preventDefault();
+
+    try {
+      const res = await axios.post("http://localhost:8080/api/auth/login", {
+        email,
+        password,
+      });
+
+      localStorage.setItem("token", res.data.token);
+      navigate("/dashboard");
+    } catch (error) {
+      console.error(error);
+      alert("Invalid credentials");
+    }
+  }
+
+  async function handleRegister(e) {
+    e.preventDefault();
+
+    try {
+      await axios.post("http://localhost:8080/api/auth/signup", {
+        name,
+        email,
+        password,
+      });
+
+      alert("Registration successful! You can login now.");
+      navigate("/");
+    } catch (error) {
+      console.error(error);
+      alert("Registration failed");
+    }
+  }
+
   return (
-    <div>
-      
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <form className="flex flex-col gap-4 m-auto items-start p-8 py-12 w-80 sm:w-[352px] text-gray-500 rounded-lg shadow-xl border border-gray-200 bg-white" onSubmit={state==="register"?handleRegister : handleLogin}>
+        <p className="text-2xl font-medium m-auto">
+          <span className="text-indigo-500">CampusLink</span>{" "}
+          {state === "login" ? "Login" : "Sign Up"}
+        </p>
+        {state === "register" && (
+          <div className="w-full">
+            <p>Name</p>
+            <input
+              onChange={(e) => setName(e.target.value)}
+              value={name}
+              placeholder="type here"
+              className="border border-gray-200 rounded w-full p-2 mt-1 outline-indigo-500"
+              type="text"
+              required
+            />
+          </div>
+        )}
+        <div className="w-full ">
+          <p>Email</p>
+          <input
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+            placeholder="type here"
+            className="border border-gray-200 rounded w-full p-2 mt-1 outline-indigo-500"
+            type="email"
+            required
+          />
+        </div>
+        <div className="w-full ">
+          <p>Password</p>
+          <input
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+            placeholder="type here"
+            className="border border-gray-200 rounded w-full p-2 mt-1 outline-indigo-500"
+            type="password"
+            required
+          />
+        </div>
+        {state === "register" ? (
+          <p>
+            Already have account?{" "}
+            <span
+              onClick={() => setState("login")}
+              className="text-indigo-500 cursor-pointer"
+            >
+              click here
+            </span>
+          </p>
+        ) : (
+          <p>
+            Create an account?{" "}
+            <span
+              onClick={() => setState("register")}
+              className="text-indigo-500 cursor-pointer"
+            >
+              click here
+            </span>
+          </p>
+        )}
+        <button className="bg-indigo-500 hover:bg-indigo-600 transition-all text-white w-full py-2 rounded-md cursor-pointer">
+          {state === "register" ? "Create Account" : "Login"}
+        </button>
+      </form>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
